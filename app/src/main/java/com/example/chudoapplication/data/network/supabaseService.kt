b3ef0b3314de4b125be8616f33781226eaec379c
Teriton <subochartem2007@mail.ru>
2025-02-19 12:56:49 +0300

    Added sign in button

--- app/src/main/java/com/example/chudoapplication/data/network/supabaseService.kt
+++ app/src/main/java/com/example/chudoapplication/data/network/supabaseService.kt
@@ -26,5 +26,6 @@ val supabase = createSupabaseClient(
     supabaseUrl = "https://rjbskedzyfdmynpbwygf.supabase.co",
     supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqYnNrZWR6eWZkbXlucGJ3eWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3Njc4NjIsImV4cCI6MjA1NTM0Mzg2Mn0.Uk8fyMjDlLtamK540Nhp1wIbAX7DYza-X-kD38qMe_8"
 ) {
+    install(Auth)
     install(Postgrest)
 }
--- app/src/main/java/com/example/chudoapplication/presentation/signIn/SignInScreen.kt
+++ app/src/main/java/com/example/chudoapplication/presentation/signIn/SignInScreen.kt
@@ -48,7 +48,6 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.chudoapplication.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
